7ac789aafbd3521a572dd0c2af0ddda5448628d0
Kanika Sharma <kanikash4@gmail.com>
2020-06-10 18:31:04 +0530

    Added window onload and resize

--- play.js
+++ play.js
@@ -1,7 +1,15 @@
+var mazeCanvas = document.getElementById("mazeCanvas");
+var ctx = mazeCanvas.getContext("2d");
+var sprite;
+var finishSprite;
+var maze, draw, player;
+var cellSize;
+var difficulty;
+
 
 
 function makeMaze() {
-  
+  //document.getElementById("mazeCanvas").classList.add("border");
   if (player != undefined) {
     player.unbindKeyDown();
     player = null;
@@ -15,4 +23,83 @@ function makeMaze() {
   if (document.getElementById("mazeContainer").style.opacity < "100") {
     document.getElementById("mazeContainer").style.opacity = "100";
   }
+}
+
+window.onload = function() {
+  let viewWidth = $("#view").width();
+  let viewHeight = $("#view").height();
+  if (viewHeight < viewWidth) {
+    ctx.canvas.width = viewHeight - viewHeight / 100;
+    ctx.canvas.height = viewHeight - viewHeight / 100;
+  } else {
+    ctx.canvas.width = viewWidth - viewWidth / 100;
+    ctx.canvas.height = viewWidth - viewWidth / 100;
+  }
+
+  //Load and edit sprites
+  var completeOne = false;
+  var completeTwo = false;
+  var isComplete = () => {
+    if(completeOne === true && completeTwo === true)
+       {
+         console.log("Runs");
+         setTimeout(function(){
+           makeMaze();
+         }, 500);         
+       }
+  };
+  sprite = new Image();
+  sprite.src =
+    "https://image.ibb.co/dr1HZy/Pf_RWr3_X_Imgur.png" +
+    "?" +
+    new Date().getTime();
+  sprite.setAttribute("crossOrigin", " ");
+  sprite.onload = function() {
+    sprite = changeBrightness(1.2, sprite);
+    completeOne = true;
+    console.log(completeOne);
+    isComplete();
+  };
+
+  finishSprite = new Image();
+  finishSprite.src = "https://image.ibb.co/b9wqnJ/i_Q7m_U25_Imgur.png"+
+  "?" +
+  new Date().getTime();
+  finishSprite.setAttribute("crossOrigin", " ");
+  finishSprite.onload = function() {
+    finishSprite = changeBrightness(1.1, finishSprite);
+    completeTwo = true;
+    console.log(completeTwo);
+    isComplete();
+  };
+  
+};
+
+window.onresize = function() {
+  let viewWidth = $("#view").width();
+  let viewHeight = $("#view").height();
+  if (viewHeight < viewWidth) {
+    ctx.canvas.width = viewHeight - viewHeight / 100;
+    ctx.canvas.height = viewHeight - viewHeight / 100;
+  } else {
+    ctx.canvas.width = viewWidth - viewWidth / 100;
+    ctx.canvas.height = viewWidth - viewWidth / 100;
+  }
+  cellSize = mazeCanvas.width / difficulty;
+  if (player != null) {
+    draw.redrawMaze(cellSize);
+    player.redrawPlayer(cellSize);
+  }
+};
+
+function rand(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
